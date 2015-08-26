d319968fe6c72d0a9caa09e806c6c10a976ba446
wan9dj <786549629@qq.com>
2015-08-26 16:08:42 +0800

    v0.2

--- robot.js
+++ robot.js
@@ -1,13 +1,20 @@
 var superagent = require('superagent');
 var cheerio = require('cheerio');
-//var hModel = require('./models/db').hModel;
+var fs = require('fs');
+var hModel = require('./models/db').hModel;
 
 function saveData(data){
-	var surl = 'http://www.cilizhushou.com/search/'+hdatas[d].fan+'/1';
+	var surl = 'http://www.cilizhushou.com/search/'+data.fan+'/1';
 	superagent.get(surl).end(function(err,sres){
+		if(err){
+			return; //出现错误就返回
+		}
 		var $ = cheerio.load(sres.text);
 		var goturl = $('.x-item .tail .title').eq(0).attr('href');
-		if(!goturl){ return console.log('no url');}
+		if(!goturl){
+		 fs.writeFileSync('Robot.log','没有ed2k,搜索内容为: '+data.fan+'\r\n\r\n',{encoding:'utf-8',flag:'a'},function(){});
+		 return;
+		}
 		var h = new hModel({
 			title:data.title,
 			fan:data.fan,
@@ -26,14 +33,17 @@ function getJavFan(){
 	var timer = setInterval(function(){
 		superagent.get('http://jav-fan.com/'+(page==1?'':'page/'+page)).end(function(err,sres){
 			if(err){
-				return console.log(err);
+				return;
 			}
 			clearTimeout(timeout); //如果还有有后续页，就不关掉查找器
 			var $ = cheerio.load(sres.text);
 			$('#content .post').each(function(idx,ele){
 				var title=$(".posttitle a",ele).text();
 				var fan = title.match(/\[[^\]]+\]/g);
-				if(!fan){ return console.log('没有番号');} //如果没有番号就返回
+				if(!fan){
+					fs.writeFileSync('Robot.log','标题中没有番号,标题为: '+title+'\r\n\r\n',{encoding:'utf-8',flag:'a'},function(){});
+					return;
+				} //如果没有番号就返回
 				fan = fan[0].replace(/\[/g,'').replace(/\]/g,'');
 				var img = $(".entry img",ele).attr('src');
 				var tags = [];
@@ -59,7 +69,7 @@ function getJavFan(){
 function getJavBus(){
 	superagent.get('http://www.javbus.co/genre').end(function(err,sres){
 		if(err){
-			return console.log(err);
+			return;
 		}
 		var $ = cheerio.load(sres.text);
 
@@ -75,12 +85,11 @@ function getJavBus(){
 		$(".genreitem").each(function(idx,ele){
 			var page = 1;
 			var tags = [$('a',ele).text()]; // 数据的类型
-			var url =$('a',ele).attr('href')+(page==1?'':'/'+page);
-			console.log('url :'+url)
+			var gurl =$('a',ele).attr('href')+(page==1?'':'/'+page);
 			var timer = setInterval(function(){// 搜索每个类型里面的所有页数
-				superagent.get($('a',ele).attr('href')+(page==1?'':'/'+page)).end(function(err,sres){ 
+				superagent.get(gurl).end(function(err,sres){ 
 					if(err){
-						return console.log(err);
+						return;
 					}
 					clearTimeout(timeout); //如果还有有后续页，就不关掉查找器
 					var $ = cheerio.load(sres.text);
@@ -105,37 +114,6 @@ function getJavBus(){
 		
 		});
 	}); 
-
-	/*
-	var page = 1;
-	var timer = setInterval(function(){
-		superagent.get('http://javbus.co/'+(page==1?'':'page/'+page)).end(function(err,sres){
-			if(err){
-				return console.log(err);
-			}
-			clearTimeout(timeout); //如果还有有后续页，就不关掉查找器
-			var $ = cheerio.load(sres.text);
-			$('#content .post').each(function(idx,ele){
-				var title=$(".posttitle a",ele).text();
-				var fan = title.match(/\[[^\]]+\]/g);
-				if(!fan){ return console.log('没有番号');} //如果没有番号就返回
-				fan = fan[0].replace(/\[/g,'').replace(/\]/g,'');
-				var img = $(".entry img",ele).attr('src');
-				var h = {
-					title:title,
-					fan:fan,
-					img:img,
-					tags:tags
-				}
-				saveData(h);
-			})
-		});
-		var timeout = setTimeout(function(){ //这里是用来当没有后续页数的时候就关掉定时查找器
-			clearInterval(timer);
-		},600000);
-		page++;
-	},60000)
-	*/
 }
 getJavBus();
 getJavFan();
